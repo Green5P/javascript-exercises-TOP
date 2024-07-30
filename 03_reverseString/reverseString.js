c516ffa0e7dabee5e7f395a255a597fa79b807cc
Ngwa Precious <ngwap345@gmail.com>
2024-07-30 16:51:02 +0100

    Change the funtion to a Loop function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
+// const reverseString2 = function (myString) {
+//   return myString.split("").reverse().join("");
+// };
+
 const reverseString = function (myString) {
-  return myString.split("").reverse().join("");
+  stringLength = myString.length - 1;
+  let newString = "";
+  for (let i = stringLength; i >= 0; i--) {
+    newString += myString[i];
+    // stringLength--;
+  }
+  return newString;
 };
 
 // Do not edit below this line
