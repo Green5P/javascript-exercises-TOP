dc98a269d955124c163f9c5162531ee1a3077528
Ngwa Precious <ngwap345@gmail.com>
2024-07-31 13:13:23 +0100

    Edit function to remove special values from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (myArray, ...constant) {
+  return myArray.filter(function (value) {
+    return !constant.includes(value);
+  });
 };
+
+// const removeFromArray = function(array, ...args) {
+//   return array.filter(val => !args.includes(val))
+// }
 
 // Do not edit below this line
 module.exports = removeFromArray;
