e669b550a564c5ab5ad2f627ea6eab1e682b5d96
Ngwa Precious <ngwap345@gmail.com>
2024-07-31 14:03:16 +0100

    Create Funtion to sum numbers between two positive integers including them.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function (firstNumber, secondNumber) {
+  let result = 0;
+  for (let i = firstNumber; i <= secondNumber; i++) {
+    result += i;
+  }
+  return (sum = result);
 };
 
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 // Do not edit below this line
 module.exports = sumAll;
