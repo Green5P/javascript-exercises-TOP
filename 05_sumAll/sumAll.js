3d44f8d18ed8909a31a89307a9aeec4cb4ebfdcd
Ngwa Precious <ngwap345@gmail.com>
2024-07-31 14:29:35 +0100

    Add if statement to return error if any number is not an Integer.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,6 +2,8 @@ const sumAll = function (firstNumber, lastNumber) {
   let result = 0;
   if (firstNumber < 0 || lastNumber < 0) {
     return "ERROR";
+  } else if (firstNumber % 1 != 0 || lastNumber % 1 != 0) {
+    return "ERROR";
   } else {
     if (firstNumber > lastNumber) {
       for (let i = lastNumber; i <= firstNumber; i++) {
@@ -18,17 +20,5 @@ const sumAll = function (firstNumber, lastNumber) {
   }
 };
 
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
 // Do not edit below this line
 module.exports = sumAll;
