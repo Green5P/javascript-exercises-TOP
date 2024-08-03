a9b69fcc15d8d8bc5b6d219357b1210775e7067f
Ngwa Precious <ngwap345@gmail.com>
2024-08-03 15:56:35 +0100

    Edit If statement in funtion

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -2,13 +2,13 @@ const palindromes = function (myString) {
   const myReg = /[a-zA-Z]/g;
   if (
     myReg.test(
-      myString[myString - 1]
+      myString[myString.length - 1]
     ) /* myReg.test(char) is used to check if char is the regular expression 'myReg' */ &&
-    myString[0] === myString[myString.length - 2] // Compares if the last character in the string is thesame as the first.
+    myString[0] === myString[myString.length - 1] // Compares if the last character in the string is thesame as the first.
   ) {
     return true;
-  } else if (myString[0] === myString[myString.length - 1]) {
-    return true;
+  } else {
+    return false;
   }
 };
 
