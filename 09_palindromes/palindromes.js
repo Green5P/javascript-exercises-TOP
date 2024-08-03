2689e784be35bed8c8f0a198567e87d40f4587f8
Ngwa Precious <ngwap345@gmail.com>
2024-08-03 14:13:38 +0100

    Add comments

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,8 +1,10 @@
 const palindromes = function (myString) {
   const myReg = /[a-zA-Z]/g;
   if (
-    myReg.test(myString[myString - 1]) &&
-    myString[0] === myString[myString.length - 2]
+    myReg.test(
+      myString[myString - 1]
+    ) /* myReg.test(char) is used to check if char is the regular expression 'myReg' */ &&
+    myString[0] === myString[myString.length - 2] // Compares if the last character in the string is thesame as the first.
   ) {
     return true;
   } else if (myString[0] === myString[myString.length - 1]) {
