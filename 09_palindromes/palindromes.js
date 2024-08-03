834ab5ba897b3776d840c36e8b2ea7e8cab84fd9
Ngwa Precious <ngwap345@gmail.com>
2024-08-03 14:13:17 +0100

    Add funtion to test if word is a Palindrom

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (myString) {
+  const myReg = /[a-zA-Z]/g;
+  if (
+    myReg.test(myString[myString - 1]) &&
+    myString[0] === myString[myString.length - 2]
+  ) {
+    return true;
+  } else if (myString[0] === myString[myString.length - 1]) {
+    return true;
+  }
 };
 
 // Do not edit below this line
